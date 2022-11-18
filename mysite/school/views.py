2055e887fa6bd51445d143cc0b9403eb8f9fc2de
IDL13 <leonov.ilya.02@gmail.com>
2022-11-18 23:19:23 +0300

    rollback

--- mysite/school/views.py
+++ mysite/school/views.py
@@ -19,27 +19,22 @@ def input(request):
         acounting = Accounting.objects.all()
         store = Store.objects.all()
 
+        acounting = Accounting.objects.all()
+        store = Store.objects.all()
+
         user = authenticate(request, login=log, password=pas)
         login(request, user)
         if adres == "1" and job == "1":
-            request.session.set_expiry(500)
-            request.session['pause'] = True
             return render(request, 'school/Okt_zavh.html', {'acounting': acounting, 'store': store})
         elif adres == "1" and job == "2":
-            request.session.set_expiry(500)
-            request.session['pause'] = True
             return render(request, 'school/Okt_admin.html', {'acounting': acounting, 'store': store})
         elif adres == "1" and job == "3":
-            request.session.set_expiry(500)
-            request.session['pause'] = True
             return render(request, 'school/Okt_HDR.html', {'acounting': acounting, 'store': store})
         
 def storege (request):
     if request.method == 'GET':
         store = StoreForm()
         store = Store.objects.all()
-        request.session.set_expiry(500)
-        request.session['pause'] = True
         return render(request, 'storege.html', {'store': store})
     else:
         return redirect('/')
@@ -47,8 +42,6 @@ def storege (request):
 def add(request):
     if request.method == "GET":
         add_inf = AddForm()
-        request.session.set_expiry(500)
-        request.session['pause'] = True
         return render(request, 'add.html', {"add": add_inf,} )
     if request.method == "POST":
         form = AddForm(request.POST)
