449d9a6c7ebfe8b274ce6804ed319a9ac6898cad
IDL13 <leonov.ilya.02@gmail.com>
2023-04-03 00:09:06 +0300

    Group

--- mysite/school/forms.py
+++ mysite/school/forms.py
@@ -4,9 +4,9 @@ from .models import *
 from django.contrib.auth.forms import AuthenticationForm
 from django.forms import ModelForm, TextInput
 
-class AuthorizationForm(forms.Form):
-    adress = forms.ModelChoiceField(empty_label = 'Выберите здание',queryset = Adress.objects.all(), label = '')
-    jobtitle = forms.ModelChoiceField(empty_label = 'Выберете вашу должность', queryset = User.objects.all(), label = '')
+# class AuthorizationForm(forms.Form):
+#     adress = forms.ModelChoiceField(empty_label = 'Выберите здание',queryset = Adress.objects.all(), label = '')
+#     jobtitle = forms.ModelChoiceField(empty_label = 'Выберете вашу должность', queryset = User.objects.all(), label = '')
 
 
 class AccountingForm(forms.Form):
@@ -47,9 +47,9 @@ class AddAccountingForm(forms.ModelForm):
         fields = ['users','technincs', 'tecNumber']
             
 
-class LoginUserForm(AuthorizationForm):
-    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password = forms.CharField(label='Пароль', widget=forms.TextInput(attrs={'class': 'form-input'}))
+# class LoginUserForm(AuthorizationForm):
+#     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+#     password = forms.CharField(label='Пароль', widget=forms.TextInput(attrs={'class': 'form-input'}))
 
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop('request', None)
