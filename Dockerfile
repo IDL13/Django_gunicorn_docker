2991e46b9b20b88471ad7382bc145871fb662dce
IDL13 <leonov.ilya.02@gmail.com>
2023-11-18 18:09:43 +0300

    docker_fix

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
-FROM python:3.10.5
+FROM python
 
 RUN pip install --upgrade pip
 
-COPY ./requirements.txt .
+COPY ./requirements.txt ./
 RUN pip install -r requirements.txt
 
 COPY ./mysite /app
-
 
 WORKDIR /app
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+# cd ..
+# cd ./venv/bin/
+# . activate.sh --no-input
+# cd /app
 
-gunicorn mysite.wsgi:application --bind 0.0.0.0:8000
+# pip install django --no-input
+# pip install python_dotenv --no-input
+# pip insatll psycopg2 --no-input
+# pip insatll qrcode --no-input
+# pip install bs4 --no-input
+
+python3 manage.py migrate --no-input
+python3 manage.py collectstatic --no-input
+
+# gunicorn mysite.wsgi:application --bind 0.0.0.0:8085
+python3 manage.py runserver 0.0.0.0:8000
